2e8060e92d4529192f6ed3409d282b902beb5d87
Syzygianinfern0 <spsharan2000@gmail.com>
2019-05-18 20:39:43 +0530

    Frozen Lake done

--- 2. OpenAI Task.py
+++ 2. OpenAI Task.py
@@ -1,9 +1,48 @@
 import gym
-env = gym.make('MountainCarContinuous-v0')
-env.reset()
+import numpy as np
+from time import sleep
 
-for _ in range(500):
+env = gym.make('FrozenLake8x8-v0')
+Q = np.zeros([env.observation_space.n, env.action_space.n])
+# Declaring Params
+eta = .6
+gma = .9
+episodes = 5000
+rewards = []
+
+for i in range(episodes):
+    s = env.reset()
+    rAll = 0
+    d = False
+    j = 0
+
+    # while j < 99:
+    #     env.render()
+    #     j += 1
+    #
+    #     a = np.argmax(Q[s, :] + np.random.randn(1, env.action_space.n) * (1. / (i + 1)))
+    #     s1, r, d, _ = env.step(a)
+    #     Q[s, a] = (1 - eta) * Q[s, a] + eta * (r + gma * np.max(Q[s1, :]))
+    #
+    #     rAll += r
+    #     s = s1
+    #     if d:
+    #         break
+    # Reset environment
+    s = env.reset()
+    d = False
+    # The Q-Table learning algorithm
+    while d != True:
+        env.render()
+        # Choose action from Q table
+        a = np.argmax(Q[s, :] + np.random.randn(1, env.action_space.n) * (1. / (i + 1)))
+        # Get new state & reward from environment
+        s1, r, d, _ = env.step(a)
+        # Update Q-Table with new knowledge
+        Q[s, a] = Q[s, a] + eta * (r + gma * np.max(Q[s1, :]) - Q[s, a])
+        s = s1
+        sleep(0.5)
+    # Code will stop at d == True, and render one state before it
+    rewards.append(rAll)
     env.render()
-    env.step(env.action_space.sample())
-
-# print(gym.envs.registry.all())
+print("Average Reward : ", sum(rewards) / episodes)
